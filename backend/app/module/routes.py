d1db026f89120931ba62a8e88ad8f6bd500b1951
Mathana Mathav <62739618+mathanamathav@users.noreply.github.com>
2023-02-19 22:44:47 +0530

    code clean up and setup 2 requirements  and 2 virtual env

--- backend/app/module/routes.py
+++ backend/app/module/routes.py
@@ -1,9 +1,12 @@
-from flask import request, jsonify, make_response
+from flask import request, jsonify , make_response
 from app import app
-import pickle
-from .model import LSTM_Model
 
 allowed_cities = ['Warnagal','Adilabad','Karimnagar','Khammam','Nizamabad']
+
+@app.route("/", methods=['GET'])
+def index():
+    data = {"text": "Hello World"}
+    return (jsonify(data))
 
 @app.route("/api/aqi/getMonthlyPredictions", methods=['POST'])
 def getMonthlyAqiPredictions():
@@ -11,16 +14,7 @@ def getMonthlyAqiPredictions():
     city=request.json.get('City')
     if city not in allowed_cities:
         return make_response({"Error":"Invalid City Input Given"},500)
-    try:
-        picklefile=open(r'E:/NASSCOMM Hackathon/AQI-Heatwave-Forecaster-Application/backend/app/module/models/adilabad_AQI_LSTM.pickle','rb')
-        print("Here")
-        model = pickle.load(picklefile)
-        picklefile.close()
-        forecasted=model.forecast_future(15)
-        print(forecasted)
-    except Exception as e:
-            print(e)
-            return make_response({"Error":"Model Not Found"},500)
+    
     return "Monthly Predictions-{}".format(city.lower())
 
 @app.route("/api/aqi/getDailyAQI", methods=['POST'])
@@ -40,7 +34,7 @@ def forecastAQI():
     return "Forecast AQI"
 
 @app.route("/api/aqi/forecastAQI", methods=['POST'])
-def getHistroicalAQI():
+def getHistoricalAQI():
     global allowed_cities
     city=request.json.get('City')
     if city not in allowed_cities:
