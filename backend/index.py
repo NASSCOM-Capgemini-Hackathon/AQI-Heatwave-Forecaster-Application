68f2d59af74adf12cb9a4e11e2009f4699b6b2ff
Mathana Mathav <62739618+mathanamathav@users.noreply.github.com>
2023-02-19 23:01:35 +0530

    checking vercel deployment

--- backend/index.py
+++ backend/index.py
@@ -2,4 +2,4 @@ from app import app
 from app.module.routes import *
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8800, debug=True)
+    app.run(host='0.0.0.0', port=5500, debug=True)
